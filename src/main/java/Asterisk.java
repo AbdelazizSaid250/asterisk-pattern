4a12a845fa6ca79b632ffe0390edf1f8bb029106
AbdelazizSaid250 <94939912+AbdelazizSaid250@users.noreply.github.com>
2022-05-25 00:12:38 +0200

    Update Asterisk.java

--- src/main/java/Asterisk.java
+++ src/main/java/Asterisk.java
@@ -2,17 +2,25 @@ import java.util.Scanner;
 
 public class Asterisk {
     public static void main(String[] args) {
-        System.out.print("Enter the number that you need: ");
+        System.out.print("Enter the Integer number from 1 to 1000000000: ");
         Scanner scanner = new Scanner(System.in);
 
-        int number;
+        long number;
         try {
-            number = scanner.nextInt();   // convert the inserted number to integer
+            number = scanner.nextLong();   // convert the inserted number to integer
+            if (number >= 1000000000) {
+                throw new RuntimeException("The number is higher than expected!!");
+            } else if (number < 0) {
+                throw new RuntimeException("The number is negative!!");
+            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-        Patterns.getInstance().hierarchicalPattern(number);
+        Patterns patterns = new Patterns();
+        patterns.pyramidPattern(number);
+
+//        Patterns.getInstance().hierarchicalPattern(number);
     }
 
 
