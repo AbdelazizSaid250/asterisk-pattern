1042914f053bc631f5996259c5616b85ebbe3cb3
AbdelazizSaid250 <94939912+AbdelazizSaid250@users.noreply.github.com>
2022-05-25 00:24:56 +0200

    - Unit Test

---
+++ src/test/java/PatternsTest.java
@@ -0,0 +1,29 @@
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class PatternsTest {
+    /**
+     * Method under test: {@link Patterns#pyramidPattern(long)}
+     */
+    @Test
+    @Ignore("TODO: Complete this test")
+    public void testPyramidPattern() {
+        // TODO: Complete this test.
+        //   Reason: F009 Internal error.
+        //   java.lang.NullPointerException
+        //   Please contact Diffblue through the appropriate support channel
+        //   (https://www.diffblue.com/support) providing details about this error.
+
+        // Arrange
+        // TODO: Populate arranged inputs
+        Patterns patterns = null;
+        long number = 0L;
+
+        // Act
+        patterns.pyramidPattern(number);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+}
+
